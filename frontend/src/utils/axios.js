66a35cd998874af8db5c579086057de2a20f9b11
savansr <Savansr26@gmail.com>
2025-04-28 21:33:35 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',     
+  baseURL: 'https://task-management-app-9tex.onrender.com/api',     
   headers: {
     'Content-Type': 'application/json',
   },
