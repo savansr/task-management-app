9a4e435a31b89a12404287e5fc70a56f1a5af31c
savansr <Savansr26@gmail.com>
2025-04-28 21:49:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-   origin:'https://task-management-app-9tex.onrender.com'}
+   origin:'https://task-management-5c2756lcx-savan-rathods-projects.vercel.app'}
 ));
 app.use(express.json());
 app.use(morgan('dev'));
